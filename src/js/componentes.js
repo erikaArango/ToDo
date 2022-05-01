63142799265b468645b322837cb43edea2163f9f
erikaArango <erika.arango@walmart.com>
2022-05-01 15:00:00 -0400

    apply filters

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -4,7 +4,10 @@ import { todoList} from "../index";
 //referencias en el html
 const divTodoList = document.querySelector ('.todo-list');
 const txtInput    = document.querySelector ('.new-todo');
-const btnBorrar  = document.querySelector ('.clear-completed')
+const btnBorrar   = document.querySelector ('.clear-completed');
+const ulFiltros   = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
+
 
 export const crearTodoHtml = ( todo ) => {
 
@@ -77,3 +80,39 @@ btnBorrar.addEventListener('click', () => {
         }
     }
 })
+
+ulFiltros.addEventListener('click', (event) => {
+
+   const filtro = event.target.text; 
+
+    if(!filtro){ return; }
+
+    anchorFiltros.forEach( elemt => elemt.classList.remove('selected'));
+    event.target.classList.add('selected')
+
+    console.log(event.target);
+
+    for( const elemento of divTodoList.children){
+
+        elemento.classList.remove('hidden');
+        const completado =  elemento.classList.contains('completed')
+
+        switch(filtro){
+
+            case'Pendientes':
+                if(completado) { 
+                    elemento.classList.add('hidden');
+                }
+            break;
+
+            case'Completados':
+                if(!completado) {
+                    elemento.classList.add('hidden');
+                }
+            break;
+
+        }
+    }
+
+
+} )
